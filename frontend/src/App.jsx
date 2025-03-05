d4240a354216265ea3f338dbad8eaa548d236551
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-05 12:17:27 +0200

    Add EditProductPage route to an App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// pages & components
+// Pages & Components
 import Home from "./pages/HomePage";
 import AddProductPage from "./pages/AddProductPage.jsx";
 import Navbar from "./components/Navbar";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProductPage from "./pages/ProductPage";
+import EditProductPage from "./pages/EditProductPage";
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
               <Route path="/" element={<Home />} />
               <Route path="/products/:id" element={<ProductPage/>} />
               <Route path="/add-product" element={<AddProductPage />} />
+              <Route path="/edit-product/:id" element={<EditProductPage />} />
               <Route path='*' element={<NotFoundPage />} />
             </Routes>
           </div>
